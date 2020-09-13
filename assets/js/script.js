2b56cc195403f292b1ce9b3204a4f0e7982af8d7
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-09-13 14:27:39 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// /* Section for Naseem */
+
+
+
+// /* End of Section for Naseem */
+
+// /* Section for Kiran */
+
+
+
+// /* End of Section for Kiran */
+
+// /* Section for Abhinav */
+
+
+
+// /* End of Section for Abhinav */
+
+// /* Section for Ajal */
+
+
+
+// /* End of Section for Ajal */
