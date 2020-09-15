dea55538eb83541af9b948a7d11949fafe6d7af6
kiranmurali93 <kiranmurali93@gmail.com>
2020-09-15 08:59:49 +0530

    Scheulde section update

---
+++ assets/data/schedule.js
@@ -0,0 +1,20 @@
+var schedule_day1 = [{
+    Time: '10:20 AM',
+    Event: 'Registration Starts and Breakfast',
+},
+{
+    Time: '11:30 AM',
+    Event: 'asd',
+}
+]
+
+var schedule_day2 = [{
+    Time: '10:20 AM',
+    Event: 'Breakfast',
+},
+{
+    Time: '11:30 AM',
+    Event: 'asd',
+},
+
+]
