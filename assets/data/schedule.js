35d78e39eab0c2ca654b1e600450a533f7db1728
kiranmurali93 <kiranmurali93@gmail.com>
2020-09-18 23:05:48 +0530

    schedule update

--- assets/data/schedule.js
+++ assets/data/schedule.js
@@ -20,42 +20,23 @@ var schedule_day2 = [{
 
 var before_day_events = [{
     Right:{
-        Date:'20 Sep 2020',
+        Date:'22 September 2020',
         Event:'Registration start'
     },
     left:{
-        Date:'30 Sep 2020',
+        Date:'2 October 2020',
         Event:'Registration end'
     }
 },
 {
     Right:{
-        Date:'1 Oct 2020',
-        Event:'Selection'
-    },
-    left:{
-        Date:'3 Oct 2020',
-        Event:'Payment Last Date'
-    }
-},
-{
-    Right:{
-        Date:'5 Oct 2020',
-        Event:'Participants Finalise'
-    },
-    left:{
-        Date:'10 Oct 2020',
+        Date:'8 October 2020',
         Event:'Team Formation'
-    }
-},
-{
-    Right:{
-        Date:'15 Oct 2020',
-        Event:'Finalisation of teams And Domain release'
     },
     left:{
-        Date:'17 Oct 2020',
-        Event:'Event Day'
+        Date:'15 October 2020',
+        Event:'Domain Release'
     }
 },
+
 ]
