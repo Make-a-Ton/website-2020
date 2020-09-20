22e8eb38b347e74c8e26495f0cc839e028e6ba05
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-09-20 15:56:54 +0530

    Added loading and fixed footer bug

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,23 +1,27 @@
 window.onload = () => {
     // team
-    team.forEach((ele) => {
-     
-      var card = `
-    
-    
-        <!-- Grid column -->
+    var ref = document.getElementById("team");
+    ref.innerHTML= '';
+
+    setTimeout(teamInject(), 3000);
+    function teamInject(){
+      team.forEach((ele) => {
+        var card = `
       
-          <div class="avatar mx-auto">
-            <img src="${ele.photo}" class="rounded-circle z-depth-1"
-              alt="Sample avatar">
-          </div>
-          <a href="${ele.linkedin}"<h5 class="font-weight-bold mt-4 mb-3">${ele.name}</h5>
       
-  
-      `;
-      let div = document.createElement("div");
-      div.className = "col-lg-4 col-md-6 frame";
-      var ref = document.getElementById("team");
-      div.innerHTML = card;
-      ref.appendChild(div);
-    })}
+          <!-- Grid column -->
+        
+            <div class="avatar mx-auto">
+              <img src="${ele.photo}" class="rounded-circle z-depth-1"
+                alt="Sample avatar">
+            </div>
+            <a href="${ele.linkedin}"<h5 class="font-weight-bold mt-4 mb-3">${ele.name}</h5>
+        
+    
+        `;
+        let div = document.createElement("div");
+        div.className = "col-lg-4 col-md-6 frame";
+        div.innerHTML = card;
+        ref.appendChild(div);
+      })}
+    }
