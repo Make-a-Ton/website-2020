cbef53f92d1593989db98c6562d83030e2103d8f
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-09-22 10:57:48 +0530

    added DSC Logo

--- assets/data/sponsors.js
+++ assets/data/sponsors.js
@@ -6,6 +6,11 @@ let sponsorsList = [
   {
     name: "AWS UG Kochi ",
     photo: "https://make-a-ton.github.io/website-2020/assets/img/awsug.png",
+  },
+  {
+    name: 'Developer Student Clubs, CUSAT',
+    photo: 'https://make-a-ton.github.io/website-2020/assets/img/dsc-light.png'
+
   }
 
 ];
