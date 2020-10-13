cb28748fbc37f696d4302bc1a9d1255e3d133017
Ajal <ajapoos1999@gmail.com>
2020-10-13 22:44:54 +0530

    voiceflow image

--- assets/data/sponsors.js
+++ assets/data/sponsors.js
@@ -23,4 +23,9 @@ let sponsorsList = [
     name: "Word Press Kochi",
     photo: "https://make-a-ton.github.io/website-2020/assets/img/wpkochi.png",
   },
+  // {
+  //   name: "Voiceflow",
+  //   photo:
+  //     "https://make-a-ton.github.io/website-2020/assets/img/voiceflow-logo.svg",
+  // },
 ];
